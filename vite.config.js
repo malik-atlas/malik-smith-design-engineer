4f3d6f2eda423bcdbf349326b731f728c1b660e2
malik-atlas <malikux@proton.me>
2026-06-04 11:13:02 +0000

    updated vite.svg path and config file

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite'; // Import the new plugin
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: './',
-})
+  plugins: [
+    react(),
+    tailwindcss(), // Add it here
+  ],
+});
