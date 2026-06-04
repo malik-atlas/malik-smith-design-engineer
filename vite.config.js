8685aadcb4fdc4e8499efcf86fccca983f4d4854
malik-atlas <malikux@proton.me>
2026-06-04 15:43:25 +0000

    chagned vite config file

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,11 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite'; // Import the new plugin
 
 export default defineConfig({
+  base: '/', // Ensure this matches your deployment intent
   plugins: [
     react(),
     tailwindcss(), // Add it here
   ],
 });
+
+
